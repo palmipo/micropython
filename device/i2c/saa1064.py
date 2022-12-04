f7fa0b2ad1f027028e775833c728766a93c7574c
palmipo <palmipo@free.fr>
2022-12-04 18:24:21 +0100

    ajout saa1064

---
+++ device/i2c/saa1064.py
@@ -0,0 +1,48 @@
+from devicei2c import DeviceI2C
+
+class SAA1064(DeviceI2C):
+
+    def __init__(self, address, bus):
+        super().__init__(0x38 | (address & 0x03), bus)
+
+    def controlRegister(self, dynamique_mode, digit_1_3, digit_2_4, test, intensity):
+        cmd = bytearray(2)
+        cmd[0] = 0x00 # control register
+        cmd[1] = (dynamique_mode & 0x01) | ((digit_1_3 & 0x01) << 1) | ((digit_2_4 & 0x01) << 2) | ((test & 0x01) << 3) | ((intensity & 0x07) << 4)
+        self.busi2c.send(self.adresse, cmd)
+
+    def test(self, on):
+        self.contrlRegister(1, 1, 1, on, 7)
+
+    def setIntensity(self, intensity):
+        self.controlRegister(1, 1, 1, 0, intensity)
+
+    def setDigit(self, indice, valeur):
+        cmd = bytearray(2)
+        cmd[0] = indice & 0xff
+        cmd[1] = valeur & 0xff
+        self.busi2c.send(self.adresse, cmd)
+
+    def set(self, texte):
+        cpt = 0
+
+        for i in range(0, len(texte)):
+            valeur = self.transformation(texte[cpt])
+            if ((cpt+1) < len(texte)) && (texte[cpt + 1] == '.'):
+                valeur = valeur | 0x80
+            self.setDigit(i, valeur)
+            cpt += 1
+
+    def transformation(self, valeur):
+        switch={
+            '0': return 0x3F,
+            '1': return 0x06,
+            '2': return 0x5B,
+            '3': return 0x4F,
+            '4': return 0x66,
+            '5': return 0x6D,
+            '6': return 0x7D,
+            '7': return 0x07,
+            '8': return 0x7F,
+            '9': return 0x6F,
+        }
