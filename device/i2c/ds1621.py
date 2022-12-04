f662f9c2fbc338121d1eca1a65a28137c6e30453
palmipo <palmipo@free.fr>
2022-12-04 22:01:46 +0100

    ds1621

--- device/i2c/ds1621.py
+++ device/i2c/ds1621.py
@@ -1,40 +1,53 @@
 from devicei2c import DeviceI2C
+import time
 
 class DS1621(DeviceI2C):
 
     def __init__(self, address, bus):
         super().__init__(0x48 | (address & 0x03), bus)
 
-    def getAccessTh(self):
+    def getAccess(self):
         buf = bytearray(1)
         buf[0] = 0xa1
-        return self.busi2c.transferer(self.adresse, buf)
+        msg = self.busi2c.transferer(self.adresse, buf, 4)
+        th = (1<<8) - msg[0]
+        tl = (1<<8) - msg[2]
+        return th, tl
 
-    def setAccessTh(self, valeur):
-        buf = bytearray(3)
+    def setAccess(self, th, tl):
+        buf = bytearray(5)
         buf[0] = 0xa1
-        buf[1] = ((1<<8) + valeur) & 0xff
+        buf[1] = (1<<8) + (th & 0xff)
         buf[2] = 0
+        buf[3] = (1<<8) + (tl & 0xff)
+        buf[4] = 0
         self.busi2c.send(self.adresse, buf)
         time.sleep_ms(10)
+        self.waitNVB()
 
-    def getAccessTl(self):
-        buf = bytearray(1)
-        buf[0] = 0xa2
-        return self.busi2c.transferer(self.adresse, buf)
+    def isThf(self):
+        cmd = bytearray(1)
+        cmd[0] = 0xac
+        buf = self.busi2c.transferer(self.adresse, cmd, 1)
+        return ((((buf[0] & 0x40) >> 6) != 0) ? True : False)
 
-    def setAccessTl(self, valeur):
-        buf = bytearray(3)
-        buf[0] = 0xa2
-        buf[1] = ((1<<8) + valeur) & 0xff
-        buf[2] = 0
-        self.busi2c.send(self.adresse, buf)
-        time.sleep_ms(10)
+    def isTlf(self):
+        cmd = bytearray(1)
+        cmd[0] = 0xac
+        buf = self.busi2c.transferer(self.adresse, cmd, 1)
+        return ((((buf[0] & 0x20) >> 5) != 0) ? True : False)
 
     def readOneTemperature(self):
-        buf = bytearray(1)
-        buf[0] = 0xaa
-        return self.busi2c.transferer(self.adresse, buf, 2)
+        cmd = bytearray(2)
+        cmd[0] = 0xac
+        cmd[1] = 0x01
+        self.busi2c.send(self.adresse, cmd)
+
+        time.sleep_ms(10)
+        self.waitNVB()
+
+        self.start()
+        return self.readTemperature()
 
     def start(self):
         buf = bytearray(1)
@@ -47,16 +60,16 @@ class DS1621(DeviceI2C):
         return self.busi2c.send(self.adresse, buf, 1)
         
     def readTemperature(self):
-        slope = readSlope()
-        counter = readCounter()
+        self.waitDone()
+
+        slope = self.readSlope()
+        counter = self.readCounter()
 
         buf = bytearray(1)
         buf[0] = 0xaa
         msg = self.busi2c.transferer(self.adresse, buf, 2)
 
-        t = float((1<<8) - msg[0])
-        if msg[1] != 0:
-            t += 0.5
+        t = (1<<8) - msg[0]
         return (t + ((slope - counter) / slope))
 
     def readCounter(self):
@@ -70,16 +83,15 @@ class DS1621(DeviceI2C):
         return self.busi2c.transferer(self.adresse, buf, 1)
         
     def read(self):
-        waitDone()
+        self.waitDone()
 
         buf = bytearray(1)
         buf[0] = 0xa8
         msg = self.busi2c.transferer(self.adresse, buf, 4)
+
         counter = float(msg[0])
         slope = float(msg[1])
-        t = float((1<<8) - msg[2])
-        if msg[3] != 0:
-            t += 0.5
+        t = (1<<8) - msg[2]
         return (t + ((slope - counter) / slope))
 
     def waitDone(self):
@@ -87,6 +99,7 @@ class DS1621(DeviceI2C):
         buf[0] = 0xac
         access_config = self.busi2c.transferer(self.adresse, buf, 1)
         while (access_config & 0x80) == 0:
+            time.sleep_ms(1)
             access_config = self.busi2c.transferer(self.adresse, buf, 1)
 
     def waitNVB(self):
@@ -94,4 +107,5 @@ class DS1621(DeviceI2C):
         buf[0] = 0xac
         access_config = self.busi2c.transferer(self.adresse, buf, 1)
         while (access_config & 0x10) == 0:
+            time.sleep_ms(1)
             access_config = self.busi2c.transferer(self.adresse, buf, 1)
