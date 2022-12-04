9b5794585505d80e3063b069a380d5eb58e9adb4
palmipo <55411002+palmipo@users.noreply.github.com>
2022-12-04 16:08:17 +0100

    Update test_wifi.py

--- test_wifi.py
+++ test_wifi.py
@@ -5,7 +5,7 @@ wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 mac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()
 print(mac)
-wlan.connect('batrahome', 'xxxx')
+wlan.connect('xxxx', 'xxxx')
 while not wlan.isconnected() and wlan.status() >= 0:
   print("Waiting to connect:")
   time.sleep(1)
