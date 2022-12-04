d6835256cfe6b14528137f5cb3c0458c401b8d0c
palmipo <palmipo@free.fr>
2022-12-04 16:01:17 +0100

    ajout adresse i2c device

---
+++ master/pwm/pwmmaster.py
@@ -0,0 +1,12 @@
+import rp2
+from machine import Pin, PWM
+
+class PwmMaster:
+    def __init__(self, pin):
+        self.pwm = PWM(Pin(pin))
+
+    def setFrequency(self, freq):
+        pwm.periode(1/freq)
+        
+    def setDuty(self, pourcentage):
+        pwm.duty_ns(pourcentage * self.periode / 100)
