53b997ff14090e13df0b420c72839e4ace6aa879
palmipo <55411002+palmipo@users.noreply.github.com>
2022-12-12 09:04:57 +0100

    Update test_ada772.py

--- test_ada772.py
+++ test_ada772.py
@@ -6,22 +6,28 @@ from ada772 import ADA772
 from hd44780 import HD44780
 
 
-def callback(ADA772):
-    print("callback")
+def callback(pin):
+    bt_pressed = True
 
+bt_pressed = False
 i2c = BusI2C(0, Pin(4), Pin(5))
 
 mux = PCA9548A(0, i2c)
 mux.setCanal((1<<7)|(1<<6))
 circuits = i2c.scan()
-print("circuits presents sur le bus i2c : ")
-print(circuits)
-print()
 
-lcd_io = ADA772(0, i2c, 2, callback)
+isr = Pin(2, Pin.IN, Pin.PULL_UP)
+isr.irq(callback, Pin.FALLING_EDGE)
+
+lcd_io = ADA772(0, i2c)
 lcd_io.setBackLight(1)
 
 lcd = HD44780(lcd_io)
 lcd.clear()
 lcd.home()
 lcd.writeText("Hello World !")
+
+while True:
+    if bt_pressed == True:
+        lcd_io.scrute()
+        bt_pressed = False
