6ff23acc16efd31e16788ba58b5059e97b563a81
palmipo <55411002+palmipo@users.noreply.github.com>
2022-12-12 09:21:42 +0100

    Update pia_pcf8574.py

--- master/pia/pia_pcf8574.py
+++ master/pia/pia_pcf8574.py
@@ -2,15 +2,15 @@ from pia import PIA
 from pcf8574 import PCF8574
 
 class PIA_PCF8574(PIA):
-  def __init__(self, pia):
+  def __init__(self, pcf8574):
     super().__init__()
-    self.pia = pia
+    self.pcf8574 = pcf8574
     
   def setOutput(self, value):
 #     print("setOutput port : " + str(self.port) + " valeur : " + str(hex(value)))
-    self.pia.setOLAT(value)
+    self.pcf8574.setOLAT(value)
 
   def getInput(self):
-    value = self.pia.getGPIO()
+    value = self.pcf8574.getGPIO()
 #     print("getInput port : " + str(self.port) + " valeur : " + str(hex(value)))
     return value
