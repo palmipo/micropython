d47b02b547111e478eaaf6b58f341def29f98c02
palmipo <55411002+palmipo@users.noreply.github.com>
2023-08-18 00:35:04 +0200

    Create n4dih32.py

---
+++ device/modbus/n4dih32.py
@@ -0,0 +1,19 @@
+from modbusrtu import ModbusRtu
+from modbusexception import ModbusException
+from modbusmsg03 import ModbusMsg03
+from modbusmsg06 import ModbusMsg06
+
+
+class N4DIH32:
+    def __init__(self, modbusId, rtu):
+        self.__modbusId = modbusId
+        self.__rtu = rtu
+
+    def read(self, voie):
+        fc03 = ModbusMsg03(self.__modbusId, self.__rtu)
+        return fc03.readHoldingRegisters(0x80 | (voie & 0x2F), 2)
+
+    def readAll(self):
+        fc03 = ModbusMsg03(self.__modbusId, self.__rtu)
+        return fc03.readHoldingRegisters(0x00C0, 4)
+
