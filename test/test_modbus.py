1ac2f7e845253cbb72721762731b23e8f7dc365a
palmipo <55411002+palmipo@users.noreply.github.com>
2023-08-18 00:21:06 +0200

    modbus

--- test/test_modbus.py
+++ test/test_modbus.py
@@ -1,7 +1,10 @@
 from picouart import PicoUart
-from n4dog16 import N4DOG16
+from modbusrtu import ModbusRtu
+from modbusexception import ModbusException
+from modbusmsg03 import ModbusMsg03
+from modbusmsg06 import ModbusMsg06
 
 rs485 = PicoUart(0)
 rtu = ModbusRtu(rs485)
-out = N4DOG16(0x04, rtu)
-out.close(1)
+fc03 = ModbusMsg03(0xff, rtu)
+print("adresse module : " + fc03.readHoldingRegisters(0x00FE, 0x0001))
