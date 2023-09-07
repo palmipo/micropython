dbf70f4c225a2e052fdffbab449e8d2c7dd4c8da
palmipo <palmipo@free.fr>
2023-09-07 21:34:02 +0200

    waveshare green clock

--- device/waveshare/waveshare-clock-green/waveshareclockgreen.py
+++ device/waveshare/waveshare-clock-green/waveshareclockgreen.py
@@ -2,6 +2,7 @@ from sm5166 import SM5166P
 from sm16106 import SM16106SC
 from i2cpico import I2CPico
 #from ds3231 import DS3231
+import framebuf
 
 class WaveshareClockGreen:
     def __init__(self):
@@ -10,16 +11,24 @@ class WaveshareClockGreen:
         self.i2c = I2CPico(1, 6, 7)
         #self.rtc = DS3231(0, i2c, 3)
 
-data = bytearray(4)
+buffer = bytearray(22 * 7)
+frame = framebuf.FrameBuffer(buffer, 22, 7, framebuf.MONO_HLSB)
+frame.text('H', 0, 0)
+data = [bytearray(4), bytearray(4), bytearray(4), bytearray(4), bytearray(4), bytearray(4), bytearray(4), bytearray(4)]
 clock = WaveshareClockGreen()
 clock.column.OutputEnable()
-data[0] = 0xFF
-data[1] = 0xFF
-data[2] = 0xFF
-#data[3] = 0x00
+j = 0
+for i in range(1,8):
+    data[i][0] = buffer[j]
+    j+=1
+    data[i][1] = buffer[j]
+    j+=1
+    data[i][2] = buffer[j]
+    j+=1
+    data[i][3] = 0x00
 
 while True:
-    clock.column.send(data)
-    for i in range(7):
+    for i in range(8):
+        clock.column.send(data[i])
         clock.row.setChannel(i)
         clock.column.latch()
