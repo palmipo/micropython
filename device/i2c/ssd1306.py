138fdfeeecf67f1b4a1cdd9669f8ead77da1294a
palmipo <55411002+palmipo@users.noreply.github.com>
2023-09-11 19:34:10 +0200

    Update ssd1306.py

--- device/i2c/ssd1306.py
+++ device/i2c/ssd1306.py
@@ -10,17 +10,20 @@ class SSD1306(DeviceI2C):
 
     def initDisplay(self):
         self.setDisplayOFF()
+        self.setMemoryAddressingMode(0)
+        self.setDisplayStartLine()
+        self.setSegmentRemap(1)
         self.setMultiplexRatio(self.height - 1)
+        self.setCOMOutputScanDirection(1)
         self.setDisplayOffset(0x00)
-        self.setDisplayStartLine(0x00)
-        self.setSegmentRemap(0x00)
-        self.setCOMOutputScanDirection(0x01)
-        self.SetCOMPinsHardwareConfiguration(0x00)
-        self.setContrastControl(0x0F)
-        self.setEntireDisplayON(0x00)
-        self.setNormalDisplay(0x00)
+        self.SetCOMPinsHardwareConfiguration(0x01)
         self.setDisplayClockDivideRatioOscillatorFrequency(0x80)
-        self.setChargePumpSetting(0x01)
+        self.setPrechargePeriod(1)
+        self.setVCOMHDeselectLevel(1)
+        self.setContrastControl(0x0F)
+        self.setEntireDisplayON()
+        self.setNormalDisplay()
+        self.setChargePumpSetting(1)
         self.setDisplayON()
 
     def show(self, buffer):
@@ -29,129 +32,205 @@ class SSD1306(DeviceI2C):
         self.write_data(buffer)
 
     def read_data(self):
-        return self.busi2c.recv(1)[0]
+        cmd = bytearray(1)
+        cmd[0] = 0x80
+        return self.busi2c.transfer(cmd, 1)[0]
 
     def write_data(self, buffer):
-        cmd = bytearray(1+self.width)
+        cmd = bytearray(1+len(buffer))
         cmd[0] = 0x40 # Co=0 D/C=1
-        cmd[1:] = buffer[:self.width]
+        cmd[1:] = buffer
         self.busi2c.send(self.adresse, cmd)
 
     def write_cmd(self, reg):
         cmd = bytearray(2)
         cmd[0] = 0x80 # Co=1 D/C=0
-        cmd[1] = reg
-        self.busi2c.send(self.adresse, cmd)
-
-    def write_cmd_data(self, val):
-        cmd = bytearray(2)
-        cmd[0] = 0xC0 # Co=1 D/C=0
-        cmd[1] = val & 0xFF
+        cmd[1:] = reg
         self.busi2c.send(self.adresse, cmd)
 
     def setContrastControl(self, valeur):
-        self.write_cmd(0x81)
-        self.write_cmd_data(valeur & 0xFF)
+        cmd = bytearray(2)
+        cmd[0] = 0x81
+        cmd[1] = valeur & 0xFF
+        self.write_cmd(cmd)
 
     def setChargePumpSetting(self, enable):
-        self.write_cmd(0x8D)
-        self.write_cmd_data(0x10 | ((enable & 0x01) << 2))
+        cmd = bytearray(2)
+        cmd[0] = 0x8D
+        cmd[1] = 0x10 | ((enable & 0x01) << 2)
+        self.write_cmd(cmd)
 
-    def setEntireDisplayON(self, valeur):
-        self.write_cmd(0xA4 )
-        self.write_cmd_data((valeur & 0x01))
+    def setEntireDisplayON(self):
+        cmd = bytearray(1)
+        cmd[0] = 0xA5
+        self.write_cmd(cmd)
 
-    def setNormalDisplay(self, valeur):
-        self.write_cmd(0xA6)
-        self.write_cmd_data((valeur & 0x01))
+    def setEntireDisplayOFF(self):
+        cmd = bytearray(1)
+        cmd[0] = 0xA4
+        self.write_cmd(cmd)
+
+    def setNormalDisplay(self):
+        cmd = bytearray(1)
+        cmd[0] = 0xA6
+        self.write_cmd(cmd)
+
+    def setInverseDisplay(self):
+        cmd = bytearray(1)
+        cmd[0] = 0xA7
+        self.write_cmd(cmd)
 
     def setDisplayON(self):
-        self.write_cmd(0xAF)
+        cmd = bytearray(1)
+        cmd[0] = 0xAF
+        self.write_cmd(cmd)
 
     def setDisplayOFF(self):
-        self.write_cmd(0xAE)
+        cmd = bytearray(1)
+        cmd[0] = 0xAE
+        self.write_cmd(cmd)
 
-    def continuousHorizontalScrollSetup(self, leftHorizontalScroll, startPageAddress, setTimeInterval, endPageAddress):
-        self.write_cmd(0x26 | (leftHorizontalScroll & 0x01))
-        self.write_cmd_data(0x00)
-        self.write_cmd_data(startPageAddress & 0x07)
-        self.write_cmd_data(setTimeInterval & 0x07)
-        self.write_cmd_data(endPageAddress & 0x07)
-        self.write_cmd_data(0x00)
-        self.write_cmd_data(0xFF)
+    def rightHorizontalScrollSetup(self, startPageAddress, setTimeInterval, endPageAddress):
+        cmd = bytearray(7)
+        cmd[0] = 0x26
+        cmd[1] = 0x00
+        cmd[2] = startPageAddress & 0x07
+        cmd[3] = setTimeInterval & 0x07
+        cmd[4] = endPageAddress & 0x07
+        cmd[5] = 0x00
+        cmd[6] = 0xFF
+        self.write_cmd(cmd)
 
-    def continuousVerticalHorizontalScrollSetup(self, leftHorizontalScroll, startPageAddress, setTimeInterval, endPageAddress):
-        self.write_cmd(0x28 | (verticalAndRightHorizontalScroll & 0x01))
-        self.write_cmd_data(0x28 | ((verticalAndLeftHorizontalScroll & 0x01) << 1))
-        self.write_cmd_data(startPageAddress & 0x07)
-        self.write_cmd_data(setTimeInterval & 0x07)
-        self.write_cmd_data(endPageAddress & 0x07)
-        self.write_cmd_data(verticalScrollingOffset & 0x3F)
+    def leftHorizontalScrollSetup(self, startPageAddress, setTimeInterval, endPageAddress):
+        cmd = bytearray(7)
+        cmd[0] = 0x27
+        cmd[1] = 0x00
+        cmd[2] = startPageAddress & 0x07
+        cmd[3] = setTimeInterval & 0x07
+        cmd[4] = endPageAddress & 0x07
+        cmd[5] = 0x00
+        cmd[6] = 0xFF
+        self.write_cmd(cmd)
+
+    def verticalRightHorizontalScrollSetup(self, startPageAddress, setTimeInterval, endPageAddress):
+        cmd = bytearray(6)
+        cmd[0] = 0x29
+        cmd[1] = 0x2A
+        cmd[2] = startPageAddress & 0x07
+        cmd[3] = setTimeInterval & 0x07
+        cmd[4] = endPageAddress & 0x07
+        cmd[5] = verticalScrollingOffset & 0x3F
+        self.write_cmd(cmd)
+
+    def verticalLeftHorizontalScrollSetup(self, startPageAddress, setTimeInterval, endPageAddress):
+        cmd = bytearray(6)
+        cmd[0] = 0x28 | (verticalAndRightHorizontalScroll & 0x01)
+        cmd[1] = 0x28 | ((verticalAndLeftHorizontalScroll & 0x01) << 1)
+        cmd[2] = startPageAddress & 0x07
+        cmd[3] = setTimeInterval & 0x07
+        cmd[4] = endPageAddress & 0x07
+        cmd[5] = verticalScrollingOffset & 0x3F
+        self.write_cmd(cmd)
 
     def deactivateScroll(self):
-        self.write_cmd(0x2E)
+        cmd = bytearray(1)
+        cmd[0] = 0x2E
+        self.write_cmd(cmd)
 
     def activateScroll(self):
-        self.write_cmd(0x2F)
+        cmd = bytearray(1)
+        cmd[0] = 0x2F
+        self.write_cmd(cmd)
 
     def setVerticalScrollArea(self, setRowsInTopFixedArea, setRowsInScrollArea):
-        self.write_cmd(0xA3)
-        self.write_cmd_data(setRowsInTopFixedArea & 0x3F)
-        self.write_cmd_data(setRowsInScrollArea & 0x7F)
+        cmd = bytearray(3)
+        cmd[0] = 0xA3
+        cmd[1] = setRowsInTopFixedArea & 0x3F
+        cmd[2] = setRowsInScrollArea & 0x7F
+        self.write_cmd(cmd)
 
     def setLowerColumnStartAddressForPageAddressingMode(self, setColumnStartAddressRegister):
-        self.write_cmd(setColumnStartAddressRegister & 0x0F)
-        self.write_cmd(0x10 | (setColumnStartAddressRegister >> 4))
+        cmd = bytearray(1)
+        cmd[0] = setColumnStartAddressRegister & 0x0F
+        self.write_cmd(cmd)
+        cmd[0] = 0x10 | (setColumnStartAddressRegister >> 4)
+        self.write_cmd(cmd)
 
     def setMemoryAddressingMode(self, mode):
-        self.write_cmd(0x20)
-        self.write_cmd_data(mode & 0x03)
+        cmd = bytearray(2)
+        cmd[0] = 0x20
+        cmd[1] = mode & 0x03
+        self.write_cmd(cmd)
 
     def setColumnAddress(self, columnStartAddress, columnEndAddress):
-        self.write_cmd(0x21)
-        self.write_cmd_data(columnStartAddress & 0x7F)
-        self.write_cmd_data(columnEndAddress & 0x7F)
+        cmd = bytearray(3)
+        cmd[0] = 0x21
+        cmd[1] = columnStartAddress & 0x7F
+        cmd[2] = columnEndAddress & 0x7F
+        self.write_cmd(cmd)
 
     def setPageAddress(self, pageStartAddress, pageEndAddress):
-        self.write_cmd(0x22)
-        self.write_cmd_data(pageStartAddress & 0x07)
-        self.write_cmd_data(pageEndAddress & 0x07)
+        cmd = bytearray(3)
+        cmd[0] = 0x22
+        cmd[1] = pageStartAddress & 0x07
+        cmd[2] = pageEndAddress & 0x07
+        self.write_cmd(cmd)
 
     def setDisplayStartLine(self, displayStartLine):
-        self.write_cmd(0x40 | (displayStartLine & 0x3F))
+        cmd = bytearray(1)
+        cmd[0] = 0x40 | (displayStartLine & 0x3F)
+        self.write_cmd(cmd)
 
     def setSegmentRemap(self, columnAddress0MappedtoSEG0):
-        self.write_cmd(0xA0 | (columnAddress0MappedtoSEG0 & 0x01))
+        cmd = bytearray(1)
+        cmd[0] = 0xA0 | (columnAddress0MappedtoSEG0 & 0x01)
+        self.write_cmd(cmd)
 
     def setMultiplexRatio(self, valeur):
-        self.write_cmd(0xA8)
-        self.write_cmd_data(valeur & 0x3F)
+        cmd = bytearray(2)
+        cmd[0] = 0xA8
+        cmd[1] = valeur & 0x3F
+        self.write_cmd(cmd)
 
     def setCOMOutputScanDirection(self, remappedMode):
-        self.write_cmd(0xC0 | ((remappedMode & 0x01) << 3))
+        cmd = bytearray(1)
+        cmd[0] = 0xC0 | ((remappedMode & 0x01) << 3)
+        self.write_cmd(cmd)
 
     def setDisplayOffset(self, offset):
-        self.write_cmd(0xD3)
-        self.write_cmd_data(offset & 0x3F)
+        cmd = bytearray(2)
+        cmd[0] = 0xD3
+        cmd[1] = offset & 0x3F
+        self.write_cmd(cmd)
 
     def SetCOMPinsHardwareConfiguration(self, comPinConfiguration):
-        self.write_cmd(0xDA)
-        self.write_cmd_data(((comPinConfiguration & 0x03) << 4) | 0x02)
+        cmd = bytearray(2)
+        cmd[0] = 0xDA
+        cmd[1] = ((comPinConfiguration & 0x03) << 4) | 0x02
+        self.write_cmd(cmd)
 
     def setDisplayClockDivideRatioOscillatorFrequency(self, ratio):
-        self.write_cmd(0xD5)
-        self.write_cmd_data(ratio & 0xFF)
+        cmd = bytearray(2)
+        cmd[0] = 0xD5
+        cmd[1] = ratio & 0xFF
+        self.write_cmd(cmd)
 
     def setPrechargePeriod(self, phase):
-        self.write_cmd(0xD9)
-        self.write_cmd_data(phase & 0xFF)
+        cmd = bytearray(2)
+        cmd[0] = 0xD9
+        cmd[1] = phase & 0xFF
+        self.write_cmd(cmd)
 
     def setVCOMHDeselectLevel(self, level):
-        self.write_cmd(0xDB)
-        self.write_cmd_data((level & 0x07) << 4)
+        cmd = bytearray(2)
+        cmd[0] = 0xDB
+        cmd[1] = (level & 0x07) << 4
+        self.write_cmd(cmd)
 
     def nop(self):
-        self.write_cmd(0xE3)
+        cmd = bytearray(1)
+        cmd[0] = 0xE3
+        self.write_cmd(cmd)
 
     def getStatusRegister(self):
         return self.read_data()
