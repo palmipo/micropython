6c8f9485b7344c258d54f8a490a428a478cedb41
palmipo <palmipo@free.fr>
2023-09-20 17:57:44 +0200

    waveshare clock green

--- device/i2c/ds3231.py
+++ device/i2c/ds3231.py
@@ -1,14 +1,108 @@
-from ds1307 import DS1307
-from machine import Pin
+from devicei2c import DeviceI2C
+import micropython
+import machine
 
-class DS3231(DS1307):
-    def __init__(self, address, bus, pinSQW):
-        super().__init__(address, bus)
-        self.pinSQW = Pin(pinSQW, Pin.IN, Pin.PULL_UP)
-        self.pinSQW.irq(self.callback, Pin.IRQ_FALLING)
+micropython.alloc_emergency_exception_buf(100)
 
-    def callback(pin):
-        state = machine.disable_irq()
-        self.cb()
-        machine.enable_irq(state)
+class DS3231(DeviceI2C):
+    def __init__(self, address, bus, pinSQW, cb):
+        super().__init__(0x68 | (address & 0x01), bus)
 
+        self.pinSQW = machine.Pin(pinSQW, machine.Pin.IN, machine.Pin.PULL_UP)
+        self.pinSQW.irq(handler=self.__callback__, trigger=machine.Pin.IRQ_FALLING, hard=True)
+        self.cb = cb
+
+    def __callback__(self, pin):
+#         try:
+            state = machine.disable_irq()
+            self.cb(pin)
+            machine.enable_irq(state)
+#         except exception:
+#             print('exception callback')
+
+
+    def setDayWeek(self, day):
+        cmd = bytearray(2)
+        cmd[0] = 0x03
+        cmd[1] = day & 0x07
+        self.busi2c.send(self.adresse, cmd)
+
+    def getDayWeek(self):
+        cmd = bytearray(1)
+        cmd[0] = 0x03
+        data = self.busi2c.transferer(self.adresse, cmd, 1)
+        return data[0] & 0x07
+
+    #18/01/73
+    def setDate(self, date):
+        cmd = bytearray(4)
+        cmd[0] = 0x04
+        cmd[1] = ((ord(date[0]) & 0x03) << 4) | (ord(date[1]) & 0x0F)
+        cmd[2] = ((ord(date[3]) & 0x01) << 4) | (ord(date[4]) & 0x0F)
+        cmd[3] = ((ord(date[6]) & 0x0F) << 4) | (ord(date[7]) & 0x0F)
+        self.busi2c.send(self.adresse, cmd)
+
+    def getDate(self):
+        cmd = bytearray(1)
+        cmd[0] = 0x04
+        data = self.busi2c.transferer(self.adresse, cmd, 3)
+        return str((ord(data[0]) & 0x30) >> 4) + str(ord(data[0]) & 0x0F) + "/" + str((ord(data[1]) & 0x10) >> 4) + str(ord(data[1]) & 0x0F) + "/" + str((ord(data[2]) & 0xF0) >> 4) + str(ord(data[2]) & 0x0F)
+
+    #18:25:59
+    def setTime(self, hour):
+        cmd = bytearray(4)
+        cmd[0] = 0x00
+        cmd[1] = ((ord(hour[6]) & 0x07) << 4) | (ord(hour[7]) & 0x0F)
+        cmd[2] = ((ord(hour[3]) & 0x07) << 4) | (ord(hour[4]) & 0x0F)
+        cmd[3] = ((ord(hour[0]) & 0x03) << 4) | (ord(hour[1]) & 0x0F)
+        self.busi2c.send(self.adresse, cmd)
+
+    def getTime(self):
+        cmd = bytearray(1)
+        cmd[0] = 0x00
+        data = self.busi2c.transferer(self.adresse, cmd, 3)
+        print(data)
+        return str((ord(data[2]) & 0x30) >> 4) + str(ord(data[2]) & 0x0F) + ":" + str((ord(data[1]) & 0x70) >> 4) + str(ord(data[1]) & 0x0F) + ":" + str((ord(data[0]) & 0x70) >> 4) + str(ord(data[0]) & 0x0F)
+
+    def setOut(self, etat):
+        cmd = bytearray(2)
+        cmd[0] = 0x07
+        cmd[1] = (etat & 0x01) << 7
+        self.busi2c.send(self.adresse, cmd)
+
+    def setSquareWave(self, freq):
+        cmd = bytearray(2)
+        cmd[0] = 0x07
+        cmd[1] = (freq & 0x03) | (1 << 4)
+        self.busi2c.send(self.adresse, cmd)
+
+    def setControlRegister(self, CONV, SqwareWaveFrequency, INTCN, A2IE, A1IE):
+        (OSF, EN32kHz, BSY, A2F, A1F) = self.getStatusRegister()
+        while BSY != 0:
+            (OSF, EN32kHz, BSY, A2F, A1F) = self.getStatusRegister()
+
+        cmd = bytearray(2)
+        cmd[0] = 0x0E
+        cmd[1] = ((CONV & 0x01) << 5) | ((CONV & 0x01) << 5) | ((SqwareWaveFrequency & 0x03) << 3) | ((INTCN & 0x01) << 2) | ((A2IE & 0x01) << 1) | (A1IE & 0x01)
+        self.busi2c.send(self.adresse, cmd)
+        
+    def getTemperature(self):
+        (OSF, EN32kHz, BSY, A2F, A1F) = self.getStatusRegister()
+        while BSY != 0:
+            (OSF, EN32kHz, BSY, A2F, A1F) = self.getStatusRegister()
+
+        cmd = bytearray(1)
+        cmd[0] = 0x11
+        data = self.busi2c.transferer(self.adresse, cmd, 2)
+        return ((data[0] << 2) | (data[1] >> 6)) * 0.25
+
+    def getStatusRegister(self):
+        cmd = bytearray(1)
+        cmd[0] = 0x0F
+        data = self.busi2c.transferer(self.adresse, cmd, 1)
+        OSF = (data[0] & 0x80) >> 7
+        EN32kHz = (data[0] & 0x08) >> 3
+        BSY = (data[0] & 0x04) >> 2
+        A2F = (data[0] & 0x02) >> 1
+        A1F = data[0] & 0x01
+        return (OSF, EN32kHz, BSY, A2F, A1F)
---
+++ test/test_waveshareclockgreen.py
@@ -0,0 +1,54 @@
+import ntptime
+import network
+import time
+import framebuf
+import sys
+import time
+import ntptime
+from waveshareclockgreen import WaveshareClockGreen
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+addr_mac = wlan.config('mac')
+for b in addr_mac:
+    print(hex(b))
+print(wlan.config('hostname'))
+
+wlan.connect('domoticus', '9foF2sxArWU5')
+while not wlan.isconnected() and wlan.status() >= 0:
+    time.sleep(1)
+time.sleep(10)
+
+width = 256
+height = 7
+clock = WaveshareClockGreen(width, height)
+clock.column.OutputEnable()
+
+
+ntptime.settime()
+# Year, Month、Day, Hour, Minutes, Seconds, DayWeek, DayYear
+data_tuple = time.localtime()
+laDate = str(data_tuple[2])+'/'+str(data_tuple[1])+'/'+str(data_tuple[0])
+lHeure = str(data_tuple[3])+':'+str(data_tuple[4])+':'+str(data_tuple[5])
+clock.rtc.setDate(laDate)
+clock.rtc.setTime(lHeure)
+
+buffer = bytearray(width * height // 8)
+frame = framebuf.FrameBuffer(buffer, width, height, framebuf.MONO_HMSB) # 154 bits / 20 octets
+frame.fill(0)
+frame.text(clock.rtc.getTime(), 0, 0, 1)
+clock.show(buffer, 0, 0)
+
+# i=0
+# while True:
+#     clock.show(buffer, 0, 0)
+#     frame.scroll(-1,0)
+#     if i >= width:
+#         i=0
+#         frame.fill(0)
+#         temp = clock.rtc.getTemperature()
+#         print(temp)
+#         frame.text(str(float(temp)) + ' degres Celsus', 10, 0)
+#     i+=1
+#     print(i)
+        
