40da71fd9fb20121417646634e81d7a004bc6a44
palmipo <55411002+palmipo@users.noreply.github.com>
2024-03-08 19:48:11 +0100

    Create uartraspi.py

---
+++ master/uart/uartraspi.py
@@ -0,0 +1,25 @@
+import serial
+from uartbus import UartBus
+
+class UartRaspi(UartBus):
+    def __init__(self, device, bdrate):
+        self.__uart = serial.Serial(bus, baudrate=bdrate, timeout=3.0)
+        self.__uart.init(bdrate, bits=8, parity=None, stop=1)
+        
+    def send(self, cmd):
+        self.__uart.write(cmd)
+
+    def recv(self, n_byte):
+        rxData = self.__uart.read(n_byte)
+        return rxData
+
+    def recv(self):
+        rxData = self.__uart.readline()
+        return rxData
+
+    def transfert(self, cmd, n_byte):
+        self.send(cmd)
+        return self.recv(n_byte)
+
+
+raspi = UartRaspi("/dev/ttyAMA0", 115500)
