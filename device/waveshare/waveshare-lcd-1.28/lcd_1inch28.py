2dfac854680893488f7cca94f974c4580f9e125d
palmipo <palmipo@free.fr>
2024-05-14 00:02:40 +0200

    waveshare lcd1.28 et lcd1.14

---
+++ device/waveshare/waveshare-lcd-1.28/lcd_1inch28.py
@@ -0,0 +1,50 @@
+from gc9a01a import GC9A01A
+import time
+
+class LCD_1inch28(GC9A01A):
+    def __init__(self, dc, rst, cs, spi, pwm):
+
+        self.dc = dc
+        self.rst = rst
+        self.cs = cs
+        self.spi = spi
+        self.pwm = pwm
+
+#         self.cs(1)
+#         self.dc(1)
+
+        super().__init__()
+
+        self.pwm.freq(5000)
+    
+    def set_bl_pwm(self, duty):
+        self.pwm.duty_u16(duty)#max 65535
+
+    def reset(self):
+        self.rst(1)
+        time.sleep(0.01)
+        self.rst(0)
+        time.sleep(0.01)
+        self.rst(1)
+        time.sleep(0.05)
+            
+    def write_cmd(self, cmd):
+        self.cs(1)
+        self.dc(0)
+        self.cs(0)
+        self.spi.write(bytearray([cmd]))
+        self.cs(1)
+
+    def write_data(self, buf):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(bytearray([buf]))
+        self.cs(1)
+
+    def write_buffer(self, buffer):
+        self.cs(1)
+        self.dc(1)
+        self.cs(0)
+        self.spi.write(buffer)
+        self.cs(1)
--- master/spi/spipico.py
+++ master/spi/spipico.py
@@ -4,13 +4,17 @@ from spibus import SPIBus
 class SPIPico(SPIBus):
     def __init__(self, bus, sck_pin, mosi_pin, miso_pin):
         super().__init__()
-        self.spi = SPI(bus, baudrate=100_000_000, polarity=0, phase=0, bits=8, sck=sck_pin, mosi=mosi_pin, miso=miso_pin)
+        self.spi = SPI(bus, baudrate=8000000, polarity=0, phase=0, bits=8, sck=sck_pin, mosi=mosi_pin, miso=miso_pin)
 
     def send(self, cmd):
-        self.spi.write(bytearray(cmd))
+#         print(cmd)
+        self.spi.write(cmd)
     
     def recv(self, n_byte):
         return self.spi.read(n_byte, 0)
-    
-    def transferer(self, cmd, n_byte):
-        raise NotImplementedError
+
+if __name__=='__main__':
+    spi = SPIPico(1, 10, 11, None)
+    data = bytearray(1)
+    data[0] = 0xff
+    spi.send(data)
