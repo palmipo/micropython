02da85738b97788f9a2f1f06e57031dafcff6757
palmipo <palmipo@free.fr>
2024-11-22 22:14:34 +0100

    maj menach

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import time
 time.sleep(5)
 
 # Change myprogram to whatever you renamed your current main.py to.
-import device.waveshare.waveshare_green_clock.main
+import waveshare.waveshare_green_clock.main
