3ed9706cf4bf1c9ababfb57a67b4de920d1011f1
palmipo <palmipo@free.fr>
2024-11-23 07:19:05 +0100

    menach

--- device/i2c/ds1307_sqw.py
+++ device/i2c/ds1307_sqw.py
@@ -1,27 +1,11 @@
 from device.i2c.ds1307 import DS1307
-import micropython
-import machine
-
-micropython.alloc_emergency_exception_buf(100)
+from master.pia.piaisrpico import PiaIsrPico
 
 class DS1307_SQW(DS1307):
     def __init__(self, address, bus, pinSQW):
         super().__init__(address, bus)
-        self.activated = False
 
-        self.pinSQW = machine.Pin(pinSQW, machine.Pin.IN, machine.Pin.PULL_UP)
-        self.pinSQW.irq(handler=self.callback, trigger=machine.Pin.IRQ_FALLING, hard=True)
-
-    def callback(self, pin):
-        state = machine.disable_irq()
-        try:
-            self.activated = True
-        finally:
-            machine.enable_irq(state)
+        self.pinSQW = PiaIsrPico(pinSQW)
 
     def isActivated(self):
-        if self.activated == True:
-            self.activated = False
-            return True
-        else:
-            return False
+        return self.pinSQW.isActivated()
--- device/i2c/ds3231_sqw.py
+++ device/i2c/ds3231_sqw.py
@@ -1,23 +1,11 @@
 from device.i2c.ds3231 import DS3231
-import machine
+from master.pia.piaisrpico import PiaIsrPico
 
 class DS3231_SQW(DS3231):
     def __init__(self, address, bus, pinSQW):
         super().__init__(address, bus)
-        self.activated = False
-        self.pinSQW = machine.Pin(pinSQW, machine.Pin.IN, machine.Pin.PULL_UP)
-        self.pinSQW.irq(handler=self.callback, trigger=machine.Pin.IRQ_FALLING, hard=True)
 
-    def callback(self, pin):
-        state = machine.disable_irq()
-        try:
-            self.activated = True
-        finally:
-            machine.enable_irq(state)
+        self.pinSQW = PiaIsrPico(pinSQW)
 
     def isActivated(self):
-        if self.activated == True:
-            self.activated = False
-            return True
-        else:
-            return False
+        return self.pinSQW.isActivated()
