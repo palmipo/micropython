a54d6e1e9f9a86716ddf92b1da6f59e24b7c2f32
palmipo <palmipo@free.fr>
2025-03-24 14:07:21 +0100

    uart timeout

--- master/uart/uartpico.py
+++ master/uart/uartpico.py
@@ -2,9 +2,9 @@ import machine
 from interface.uartbus import UartBus
 
 class UartPico(UartBus):
-    def __init__(self, bus, bdrate, pinTx, pinRx):
+    def __init__(self, bus, bdrate, pinTx, pinRx, timeout=1000):
         self.uart = machine.UART(bus, baudrate=bdrate, tx=machine.Pin(pinTx), rx=machine.Pin(pinRx))
-        self.uart.init(bdrate, bits=8, parity=None, stop=1)
+        self.uart.init(bdrate, bits=8, parity=None, stop=1, timeout=timeout)
         
     def send(self, cmd):
         self.uart.write(cmd)
